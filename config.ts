3e20e8c665428ca655152ceeb947ea355893e48a
Cyriaque <cyriaquedelaunay@gmail.com>
2021-11-13 18:01:32 +0100

    chore: updated list

--- config.ts
+++ config.ts
@@ -52,6 +52,42 @@ const config: IConfig = {
     },
 
     {
+      name: 'Tesla Website FR',
+      folderName: 'tesla-website-fr',
+      repoUrl: 'github.com/TeslaTracker/tracking-tesla-website-fr.git',
+      baseUrl: 'https://www.tesla.com/fr_fr',
+      urls: [
+        {
+          address: '/',
+        },
+        {
+          address: '/findus/list/%services%',
+        },
+        {
+          address: '/%products%',
+        },
+        { address: '/%products%/design', shouldGetTeslaStore: true },
+      ],
+    },
+    {
+      name: 'Tesla Website CN',
+      folderName: 'tesla-website-cn',
+      repoUrl: 'github.com/TeslaTracker/tracking-tesla-website-china.git',
+      baseUrl: 'https://www.tesla.cn',
+      urls: [
+        {
+          address: '/',
+        },
+        {
+          address: '/findus/list/%services%',
+        },
+        {
+          address: '/%products%',
+        },
+        { address: '/%products%/design', shouldGetTeslaStore: true },
+      ],
+    },
+    {
       name: 'Tesla Web API',
       folderName: 'tesla-web-api',
       repoUrl: 'github.com/TeslaTracker/tracking-tesla-web-api.git',
